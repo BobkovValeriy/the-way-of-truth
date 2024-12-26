3fc1acc921d77285fb187186e5c4ce8ceb7702c4
BobkovValeriy <darkmito17051990@gmail.com>
2024-12-26 20:32:00 +0200

    license

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -1,13 +1,32 @@
 import styles from "./Page.module.scss";
 
 const Page = ({ text, background }) => {
-
   return (
     <div
       className={styles.page}
-      style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-      dangerouslySetInnerHTML={{ __html: text }}
-    />
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div
+        dangerouslySetInnerHTML={{ __html: text }}
+        className={styles.content}
+      />
+      <div className={styles.license}>
+        Copyright © 2024 Valeriy Vladimirovich Bobkov<br />
+        This work is licensed under a{" "}
+        <a
+          href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+          International License
+        </a>.
+      </div>
+    </div>
   );
 };
 
